382b8bba124db3fe49fefc579bd692cdef0e1acc
Felix <dede4861@gmail.com>
2020-06-15 23:14:56 +0200

    Library Tests

--- Treiber/src/main.cpp
+++ Treiber/src/main.cpp
@@ -5,7 +5,7 @@
 
 #define hex_1     2   //Right Pinky finger
 #define hex_2     3   //Right Ring Finger
-#define hex_4     4    //Right Middle Finger
+#define hex_4     4   //Right Middle Finger
 #define hex_8     5   //Right Index Finger
 
 #define hex_16    6   //Left Index Finger
@@ -28,16 +28,24 @@ void setup() {
   pinMode(hex_128, INPUT);
 
   Serial.begin(9600); // Baud Rate for control-serial-connnection to 9600
+  Keyboard.begin(); //init. of Keyboard Library
 }
 
 void loop() {
   
   //Read Inputs
   // Assignment of finger values and sum
-  Value = digitalRead(hex_1)*1 + digitalRead(hex_2)*2 + digitalRead(hex_4)*4 + digitalRead(hex_8)*8 + digitalRead(hex_16)*16 + digitalRead(hex_32)*32 + digitalRead(hex_64)*64 + digitalRead(hex_128)*128;
+  Value = digitalRead(hex_1)*1 + digitalRead(hex_2)*2 + digitalRead(hex_4)*4 + digitalRead(hex_8)*8;// + digitalRead(hex_16)*16 + digitalRead(hex_32)*32 + digitalRead(hex_64)*64 + digitalRead(hex_128)*128;
+
+  if(Value > 0){
 
     Serial.println(Value);
 
+    Keyboard.write(Value);
+
     delay(200);
+
+  }
+
 
 }
